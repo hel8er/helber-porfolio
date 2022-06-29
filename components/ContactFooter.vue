6cce5cdd181b18aab98e583678a36234e2489e2e
hel8er <helber-alx@mail.ru>
2022-06-29 17:05:27 +0300

    head cover

--- components/ContactFooter.vue
+++ components/ContactFooter.vue
@@ -1,5 +1,5 @@
 <template>
-<div id="footer" class="flex justify-around max-w-2xl px-4 py-4 mx-auto sm:px-8 text-gray-700 dark:text-gray-200 items-center">
+<div id="footer" class="flex justify-around max-w-2xl px-4 py-8 mx-auto sm:px-8 text-gray-700 dark:text-gray-200 items-center">
     <div class="space-x-3 text-gray-500 transition">
       <a href="https://t.me/hel8er/" title="Telegram" class="hover:text-gray-700 dark:hover:text-gray-300"><Icon name="fa-brands:telegram" /> Telegram</a>
       <a href="https://github.com/hel8er/" title="GitHub" class="hover:text-gray-700 dark:hover:text-gray-300"><Icon name="fa-brands:github" /> GitHub</a>
--- content/about.md
+++ content/about.md
@@ -3,7 +3,7 @@ navTitle: 'About'
 layout: 'full-width'
 # Custom og:image
 head.description: 'This is a custom description for Content Wind about page.'
-head.image: 'https://i.picsum.photos/id/866/536/354.jpg?hmac=tGofDTV7tl2rprappPzKFiZ9vDh5MKj39oa2D--gqhA'
+head.image: 'https://img.freepik.com/free-vector/modern-website-banner-template-with-abstract-shapes_1361-3311.jpg?w=2000'
 ---
 
 # About in dev
