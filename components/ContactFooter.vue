2822d285592f5daadea00c77d2ec93e0dbcacfbc
hel8er <helber-alx@mail.ru>
2022-06-29 14:05:08 +0300

    titles contact

--- components/ContactFooter.vue
+++ components/ContactFooter.vue
@@ -1,9 +1,9 @@
 <template>
-<div id="footer" class="flex justify-between max-w-2xl px-4 py-4 mx-auto sm:px-8 text-gray-700 dark:text-gray-200">
-<div>Контакты:</div>
+<div id="footer" class="flex justify-between max-w-2xl px-4 py-4 mx-auto sm:px-8 text-gray-700 dark:text-gray-200 items-center">
+<div class="text-gray-500 hover:text-gray-700 dark:hover:text-gray-300">Контакты:</div>
     <div class="space-x-3 text-gray-500 transition">
-      <a href="https://t.me/hel8er/" title="Telegram" class="hover:text-gray-700 dark:hover:text-gray-300"><Icon name="fa-brands:telegram" /></a>
-      <a href="https://github.com/hel8er/" title="GitHub" class="hover:text-gray-700 dark:hover:text-gray-300"><Icon name="fa-brands:github" /></a>
+      <a href="https://t.me/hel8er/" title="Telegram" class="hover:text-gray-700 dark:hover:text-gray-300"><Icon name="fa-brands:telegram" /> Telegram</a>
+      <a href="https://github.com/hel8er/" title="GitHub" class="hover:text-gray-700 dark:hover:text-gray-300"><Icon name="fa-brands:github" /> GitHub</a>
 
     </div>
 
