f30b76cbb7597f84e2ee25a1501f78d0616c5944
hel8er <helber-alx@mail.ru>
2022-07-01 10:49:57 +0300

    blog posts

---
+++ components/content/Posts.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul>
+    <li v-for="{ _path: slug, title } in blogPosts" :key="slug">
+      <NuxtLink :to="slug">{{ title }}</NuxtLink>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+const blogPosts = await queryContent('/blog')
+  .sort({ date: -1 }) // show latest articles first
+  .where({ _partial: false }) // exclude the Partial files
+  .find();
+</script>
