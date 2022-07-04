a288f358c1bd54fb0bc4a0d4dbc691134fcdadb4
hel8er <helber-alx@mail.ru>
2022-07-04 08:47:59 +0300

    - loader

--- components/content/MarkdownBlock.vue
+++ components/content/MarkdownBlock.vue
@@ -1,24 +1,4 @@
 <!-- This component is used to show Markdown code block examples -->
 <template>
-<div v-if="loading" class="loading-page">
-    <div class="loading"></div>
-      <pre><Markdown :use="$slots.default" unwrap="p" /></pre>
-  </div>
-
+  <pre><Markdown :use="$slots.default" unwrap="p" /></pre>
 </template>
-
-<script>
-  export default {
-    data: () => ({
-      loading: false,
-    }),
-    methods: {
-      start() {
-        this.loading = true
-      },
-      finish() {
-        this.loading = false
-      },
-    },
-  }
-</script>
